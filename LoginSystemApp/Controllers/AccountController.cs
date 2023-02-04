ac55cd4b614215f1bba2ac6c4f9903aa039fe31a
Akash4756 <akashzebronic92@gmail.com>
2023-02-05 02:30:54 +0530

    Add project files.

---
+++ LoginSystemApp/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using LoginSystemApp.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace LoginSystemApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        AccountDataAccess dataAccess = new AccountDataAccess();
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel login)
+        {
+            if(ModelState.IsValid)
+            {
+                int result=dataAccess.SignInCustomer(login);
+                if(result==1)
+                {
+                    var claims = new List<Claim>() {
+                    new Claim(ClaimTypes.NameIdentifier, Convert.ToString(login.Email)),
+                        new Claim(ClaimTypes.Name, login.Email)
+                        //new Claim(ClaimTypes.Role, user.Role),
+                        //new Claim("FavoriteDrink", "Tea")
+                };
+                    //Initialize a new instance of the ClaimsIdentity with the claims and authentication scheme    
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    //Initialize a new instance of the ClaimsPrincipal with ClaimsIdentity    
+                    var principal = new ClaimsPrincipal(identity);
+                    //SignInAsync is a Extension method for Sign in a principal for the specified scheme.    
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties()
+                    {
+                        IsPersistent = false
+                    });
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.error = "Invalid Login Credentials !";
+                    return View(login);
+                }
+            }
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp(SignUpModel signUp)
+        {
+            return View();
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Login");
+        }
+
+    }
+}
---
+++ LoginSystemApp/Models/AccountDataAccess.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginSystemApp.Models
+{
+    public class AccountDataAccess
+    {
+        DBConnection dB = new DBConnection();
+
+        public string SignUpCustomer(SignUpModel sign)
+        {
+            string message = string.Empty;
+            SqlCommand command = new SqlCommand("sp_insert_customers" +
+                "", dB.connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@Name",sign.Name);
+            command.Parameters.AddWithValue("@Email", sign.Email);
+            command.Parameters.AddWithValue("@Password", sign.Password);
+            command.Parameters.AddWithValue("@Mobile", sign.Mobile);
+            command.Parameters.AddWithValue("@Gender", sign.Gender);
+            command.Parameters.AddWithValue("@Address1", sign.Address1);
+            command.Parameters.AddWithValue("@Address2", sign.Address2);
+            command.Parameters.AddWithValue("@Is_Active", sign.IsActive);
+            command.Parameters.AddWithValue("@Pincode", sign.Pin);
+
+            if (dB.connection.State == System.Data.ConnectionState.Closed)
+            {
+                dB.connection.Open();
+            }
+            command.ExecuteNonQuery();
+
+            dB.connection.Close();
+            message = "Record Save Succesfully";
+            return message;
+            
+        }
+        public int SignInCustomer(LoginModel sign)
+        {
+            string message = string.Empty;
+            SqlCommand command = new SqlCommand("sp_login_customer", dB.connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@email", sign.Email);
+            command.Parameters.AddWithValue("@password", sign.Password);
+            
+
+            if (dB.connection.State == System.Data.ConnectionState.Closed)
+            {
+                dB.connection.Open();
+            }
+            int result=(int)command.ExecuteScalar();
+            
+            dB.connection.Close();
+
+            return result;
+
+        }
+    }
+}
